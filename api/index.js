2d7843336842fd658db1af52889e18c12bacb4b7
Balvant4 <balvantkushwaha44@gmail.com>
2024-06-27 09:30:02 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const PORT = process.env.PORT || 4000; // Define a default port if not set in the environment variables
